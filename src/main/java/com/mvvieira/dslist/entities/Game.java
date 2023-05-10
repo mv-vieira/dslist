0fd70cc7b5494d7b1f5b5c6b8b9197ddbd8caf83
Matheus Vicente <mv_vieira96@hotmail.com>
2023-05-10 10:24:22 -0300

    Game seed h2

--- src/main/java/com/mvvieira/dslist/entities/Game.java
+++ src/main/java/com/mvvieira/dslist/entities/Game.java
@@ -5,20 +5,24 @@ import jakarta.persistence.*;
 import java.util.Objects;
 
 @Entity
-@Table(name = "TB_GAME")
+@Table(name = "tb_game")
 public class Game {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
+
     @Column(name = "game_year")
     private Integer year;
     private String genre;
     private String platforms;
     private Double score;
     private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game(){
