8722b75d3271d5dafb5246f698f4dda518b5344a
Matheus Vicente <mv_vieira96@hotmail.com>
2023-05-10 11:14:43 -0300

    Game repository, service, controller

--- src/main/java/com/mvvieira/dslist/entities/Game.java
+++ src/main/java/com/mvvieira/dslist/entities/Game.java
@@ -25,9 +25,7 @@ public class Game {
     @Column(columnDefinition = "TEXT")
     private String longDescription;
 
-    public Game(){
-
-    }
+    public Game(){}
 
     public Game(Long id, String title, Integer year, String genre, String platforms, Double score,
                 String imgUrl, String shortDescription, String longDescription) {
